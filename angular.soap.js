17f06b0b8bc7356f7761866ec0c960ad3574e9d9
andrewmcgivery <andrew@mcgivery.com>
2016-03-29 21:24:08 -0400

    e is null in soapCallback

--- angular.soap.js
+++ angular.soap.js
@@ -13,7 +13,7 @@ angular.module('angularSoap', [])
 			
 			//Create Callback
 			var soapCallback = function(e){
-				if(e.constructor.toString().indexOf("function Error()") != -1){
+				if(e == null || e.constructor.toString().indexOf("function Error()") != -1){
 					deferred.reject("An error has occurred.");
 				} else {
 					deferred.resolve(e);
